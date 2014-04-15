0d24ed5130b2278ef2a34dce02a0036c26374cf7
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-15 00:36:46 -0300

    Configurações geradas pela IDE.

---
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/servlet/TesteServlet.java
@@ -0,0 +1,28 @@
+package br.com.mdsgpp.guiaescolaideal.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/testando")
+public class TesteServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+        out.println("Configuração ok?");
+	}
+
+}
