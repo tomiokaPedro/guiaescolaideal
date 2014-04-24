5b3fc74dd05fcdad1c17209ef79f069191bec33a
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-24 20:33:53 -0300

    Add classe CategoriaEscola.

---
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/model/CategoriaEscola.java
@@ -0,0 +1,32 @@
+package br.com.mdsgpp.guiaescolaideal.model;
+
+public class CategoriaEscola {
+	private boolean escolaPublica;
+	private String dependenciaAdministrativa;
+	private String categoriaEscolaPrivada;
+	
+	public CategoriaEscola(boolean escolaPublica){
+		this.escolaPublica = escolaPublica;
+	}
+	
+	public boolean isEscolaPublica() {
+		return escolaPublica;
+	}
+	public void setEscolaPublica(boolean escolaPublica) {
+		this.escolaPublica = escolaPublica;
+	}
+	public String getDependenciaAdministrativa() {
+		return dependenciaAdministrativa;
+	}
+	public void setDependenciaAdministrativa(String dependenciaAdministrativa) {
+		this.dependenciaAdministrativa = dependenciaAdministrativa;
+	}
+	public String getCategoriaEscolaPrivada() {
+		return categoriaEscolaPrivada;
+	}
+	public void setCategoriaEscolaPrivada(String categoriaEscolaPrivada) {
+		this.categoriaEscolaPrivada = categoriaEscolaPrivada;
+	}
+	
+	
+}
