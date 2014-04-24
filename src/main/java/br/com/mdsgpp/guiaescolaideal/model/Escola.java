07bfe6cdf61cad1bafe150d228f15b15c4dc0c8a
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-24 20:38:53 -0300

    Limpando métodos não utilizados.

--- src/main/java/br/com/mdsgpp/guiaescolaideal/model/Escola.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/model/Escola.java
@@ -1,20 +1,7 @@
 package br.com.mdsgpp.guiaescolaideal.model;
 
-import br.com.mdsgpp.guiaescolaideal.exceptions.TamanhoDeStringErradoException;
-
 
 public class Escola {
-    private String nome;
+	private CategoriaEscola categoriaEscola;
 
-    public String getNome() {
-	return nome;
-    }
-
-    public void setNome(String nome) throws TamanhoDeStringErradoException {
-	if (nome.length() > 10) {
-	    this.nome = nome;
-	} else {
-	    throw new TamanhoDeStringErradoException();
-	}
-    }
 }
