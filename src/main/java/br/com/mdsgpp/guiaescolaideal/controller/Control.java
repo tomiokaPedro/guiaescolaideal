67a797ccc8c4553685a419daf3cd399d799ed979
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-24 20:33:31 -0300

    removendo arquivos de teste.

--- src/main/java/br/com/mdsgpp/guiaescolaideal/controller/Control.java
+++
@@ -1,9 +0,0 @@
-package br.com.mdsgpp.guiaescolaideal.controller;
-
-import br.com.mdsgpp.guiaescolaideal.model.Escola;
-
-public class Control {
-    public void getEscola(Escola escola) {
-	System.out.println(escola.getNome());
-    }
-}
