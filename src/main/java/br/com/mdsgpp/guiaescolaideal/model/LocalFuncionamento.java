27c1ab0c5109bbbe8976139cb928c81e4fa92933
Felipe Marques <felipesampaio93@hotmail.com>
2014-04-24 23:02:48 -0300

    Ajuste na classe LocalFuncionamento

--- src/main/java/br/com/mdsgpp/guiaescolaideal/model/LocalFuncionamento.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/model/LocalFuncionamento.java
@@ -4,6 +4,31 @@ public class LocalFuncionamento
 {
     private String tipoLocalFuncionamento;
     private Escola escola;
+    private Local local;
+    
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+
+    public enum Local 
+    {
+        SALA_EMPRESA,UNIDADE_INTERNACAO, TEMPLO_OU_IGREJA, CASA_PROFESSOR, RANCHO, OUTROS, OUTRA_ESCOLA;
+        private LocalFuncionamento localFuncionamento;
+
+        public LocalFuncionamento getLocalFuncionamento() {
+            return localFuncionamento;
+        }
+
+        public void setLocalFuncionamento(LocalFuncionamento localFuncionamento) {
+            this.localFuncionamento = localFuncionamento;
+        }
+        
+        
+    }
 
     public Escola getEscola() {
         return escola;
