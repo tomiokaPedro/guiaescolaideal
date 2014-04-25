3a6244f80eaad3fff9d10359562583a54e238eee
viniband1 <bandeiravinicius0@live.com>
2014-04-25 18:02:10 -0300

    Mudança de pacote do TesteRegiao

--- src/test/java/br/com/mdsgpp/guiaescolaideal/model/TesteRegiao.java
+++
@@ -1,5 +0,0 @@
-package br.com.mdsgpp.guiaescolaideal.model;
-
-public class RegiaoTest {
-
-}
---
+++ src/test/java/br/com/mdsgpp/guiaescolaideal/model/teste/TesteRegiao.java
@@ -0,0 +1,15 @@
+package br.com.mdsgpp.guiaescolaideal.model.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteRegiao {
+    @Before
+    
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
