10f4cc10f76a79a404406b40dde7b33334540481
Tainara Reis <tainara.s.r@hotmail.com>
2014-04-24 22:27:17 -0300

    Atributos da classe Endereco

--- src/main/java/br/com/mdsgpp/guiaescolaideal/model/Endereco.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/model/Endereco.java
@@ -2,4 +2,10 @@ package br.com.mdsgpp.guiaescolaideal.model;
 
 public class Endereco {
 
+	private String bairro;
+	private String rua;
+	private int cep;
+	private String numero;
+	private String complemento;
+	
 }
