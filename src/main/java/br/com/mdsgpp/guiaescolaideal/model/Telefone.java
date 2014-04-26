2797e3ab49347262401bbdb75280cfa6afa1836e
viniband1 <bandeiravinicius0@live.com>
2014-04-26 03:38:22 -0300

    Criando classe Telefone

---
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/model/Telefone.java
@@ -0,0 +1,13 @@
+package br.com.mdsgpp.guiaescolaideal.model;
+
+public class Telefone {
+   private String telefone;
+
+public String getNumero() {
+    return telefone;
+}
+
+public void setNumero(String numero) {
+    this.telefone = numero;
+}
+}
