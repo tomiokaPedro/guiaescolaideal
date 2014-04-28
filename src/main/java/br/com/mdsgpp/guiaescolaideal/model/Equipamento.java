74f115bc98a7ea192c2e5f31be40c2b2f6b0346a
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-27 21:31:22 -0300

    Utilizando uma classe "normal" em vez de enum.

--- src/main/java/br/com/mdsgpp/guiaescolaideal/model/Equipamento.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/model/Equipamento.java
@@ -1,6 +1,21 @@
 package br.com.mdsgpp.guiaescolaideal.model;
 
-public enum Equipamento {
-	DVD, IMPRESSORA, APARELHO_SOM, PROJETOR_MULTIMEDIA, FAX, MAQUINA_FOTOGRAFICA_OU_FILMADORA, PARABOLICA, FILMADORA, COPIADORA;
-
+public class Equipamento {
+	private int codEquipamento;
+	private String aparelho;
+	
+	public int getCodEquipamento() {
+		return codEquipamento;
+	}
+	public void setCodEquipamento(int codEquipamento) {
+		this.codEquipamento = codEquipamento;
+	}
+	public String getAparelho() {
+		return aparelho;
+	}
+	public void setAparelho(String aparelho) {
+		this.aparelho = aparelho;
+	}
+	
+	
 }
