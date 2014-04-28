7682fd1ff4e50a62fd69edf71646071dfc704ee7
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-27 21:29:49 -0300

    removendo classes não utilizadas.
    erro no desenho da arquitetura;

--- src/main/java/br/com/mdsgpp/guiaescolaideal/dao/Pesquisa.java
+++
@@ -1,9 +0,0 @@
-package br.com.mdsgpp.guiaescolaideal.dao;
-
-import java.util.List;
-
-import br.com.mdsgpp.guiaescolaideal.model.Escola;
-
-public interface Pesquisa<V> {
-	public List<V> pesquisarPorEscola(Escola escola);
-}
