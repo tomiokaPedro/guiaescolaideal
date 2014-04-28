d50dfb842b65a368bee250a44827fc3791d69916
ebenezerandrade <ebenezer.andrade1@gmail.com>
2014-04-28 20:48:26 -0300

    ModalidadeEnsinoDAO
    Implementação da classe ModalidadeEnsinoDAO

---
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/dao/ModalidadeEnsinoDAO.java
@@ -0,0 +1,42 @@
+package br.com.mdsgpp.guiaescolaideal.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.mdsgpp.guiaescolaideal.model.ModalidadeEnsino;
+
+public class ModalidadeEnsinoDAO {
+
+    private Connection connection;
+
+	public ModalidadeEnsinoDAO(Connection connection) {
+		this.connection = connection;
+	}
+    
+    	public ModalidadeEnsino pesquisarPorID(int id) throws SQLException{
+    	    
+    	    String sql = "select * from modalidade_ensino where COD_MODALIDADE_ENSINO = ?";
+    	    
+    	    PreparedStatement stmt = this.connection.prepareStatement(sql);  
+    	    stmt.setInt(1, id);
+
+    	    ResultSet rs = stmt.executeQuery();
+    	    
+    	    if (rs.next())
+    	    {
+    		ModalidadeEnsino modalidadeEnsino = new ModalidadeEnsino();
+    		String tipoModalidadeEnsino = rs.getString("DESCRICAO");
+    		
+    		modalidadeEnsino.setTipoModalidadeEnsino(tipoModalidadeEnsino);
+    		
+    		stmt.close();
+    		
+    		return modalidadeEnsino;
+    	    }
+    	    stmt.close();
+    	    return null;
+    	    
+    	}
+}
