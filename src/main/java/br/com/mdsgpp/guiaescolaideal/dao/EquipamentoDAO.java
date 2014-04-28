a19920ba77bdc88edc733c55143341da3c366fd7
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-27 21:52:08 -0300

    Add método de pesquida por id em EquipamentoDAO.

--- src/main/java/br/com/mdsgpp/guiaescolaideal/dao/EquipamentoDAO.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/dao/EquipamentoDAO.java
@@ -2,25 +2,43 @@ package br.com.mdsgpp.guiaescolaideal.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import br.com.mdsgpp.guiaescolaideal.model.Equipamento;
 
-public class EquipamentoDAO{
-	
+public class EquipamentoDAO {
+
 	private Connection connection;
+
 	public EquipamentoDAO(Connection connection) {
 		this.connection = connection;
 	}
 
 	public Equipamento pesquisarPorID(int id) throws SQLException {
 		String sql = "select * from equipamento where COD_EQUIPAMENTO = ?";
-		
+
 		PreparedStatement stmt = this.connection.prepareStatement(sql);
 		stmt.setInt(1, id);
-		
-		Equipamento equipamento = new Equipamento();
-		return null;
+
+		ResultSet rs = stmt.executeQuery();
+
+		if (rs.next()) {
+
+			Equipamento equipamento = new Equipamento();
+			String aparelho = rs.getString("DESCRICAO");
+
+			equipamento.setAparelho(aparelho);
+			equipamento.setCodEquipamento(id);
+
+			stmt.close();
+
+			return equipamento;
+		}
+
+		stmt.close();
+		return null; // criar uma exception ???
+
 	}
 
 }
