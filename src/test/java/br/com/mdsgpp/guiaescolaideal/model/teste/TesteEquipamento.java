bd42f8d9ce2b2bfca7f3086c3755f00bc57c55e0
Tainara Reis <tainara.s.r@hotmail.com>
2014-04-28 17:48:40 -0300

    Testando: TesteEquipamento

--- src/test/java/br/com/mdsgpp/guiaescolaideal/model/teste/TesteEquipamento.java
+++ src/test/java/br/com/mdsgpp/guiaescolaideal/model/teste/TesteEquipamento.java
@@ -1,5 +1,20 @@
 package br.com.mdsgpp.guiaescolaideal.model.teste;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import br.com.mdsgpp.guiaescolaideal.model.Equipamento;
+
+
+
 public class TesteEquipamento {
 
+	@Test
+    public void test() {
+		Equipamento equipamento = Equipamento.valueOf("IMPRESSORA");
+	
+	assertTrue(equipamento.name().equals(Equipamento.IMPRESSORA.name()));
+    }
+	
 }
