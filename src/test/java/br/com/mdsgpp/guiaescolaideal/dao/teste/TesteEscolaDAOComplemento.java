6b8f6ff1df98775e80d8535575e290e154767047
Nanson Augusto <nanson.augusto@hotmail.com>
2014-04-29 18:53:06 -0300

    Complemento do TesteEscolaDao
    o DBunit não reconhece a anotação @Test.

---
+++ src/test/java/br/com/mdsgpp/guiaescolaideal/dao/teste/TesteEscolaDAOComplemento.java
@@ -0,0 +1,33 @@
+package br.com.mdsgpp.guiaescolaideal.dao.teste;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.mdsgpp.guiaescolaideal.dao.EscolaDAO;
+
+/*
+ * Teste criado para complementar TesteEscolaDAO,
+ * pois o DbUnit não reconhece as anotações Test, Before, etc do JUnit
+ * 
+ */
+public class TesteEscolaDAOComplemento {
+	
+	EscolaDAO dao;
+	
+	@Before
+	public void setUP(){
+		this.dao = new EscolaDAO(null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPesquisarPorNomeInvalidao() throws SQLException {
+		this.dao.pesquisarPorNome("", 0, 100);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPesquisarPorNomeValorInteiroInvalido() throws SQLException {
+		this.dao.pesquisarPorNome("", 0, 100);
+	}
+}
