5327f187dac7fba9f7c457e7a0eef11d73f43308
Tainara Reis <tainara.s.r@hotmail.com>
2014-04-29 19:42:36 -0300

    Inserção da MunicípioDAO

---
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/dao/MunicipioDAO.java
@@ -0,0 +1,44 @@
+package br.com.mdsgpp.guiaescolaideal.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.mdsgpp.guiaescolaideal.model.Municipio;
+
+public class MunicipioDAO {
+
+	private Connection connection;
+	
+	public MunicipioDAO(Connection connection){
+		this.connection = connection;
+	}
+	
+	public Municipio pesquisarPorId(int id) throws SQLException{
+		
+		String sql = "select * from municipio where COD_MUNICIPIO = ?";
+		
+		PreparedStatement stmt = this.connection.prepareStatement(sql);
+		stmt.setInt(1, id);
+		
+		ResultSet rs = stmt.executeQuery();
+		
+		if(rs.next()){
+			
+			Municipio municipio = new Municipio();
+			
+			municipio.setNome(rs.getString("DESCRICAO"));
+			municipio.setCodigoDDD(rs.getString("DDD"));
+			
+			stmt.close();
+			
+			return municipio;
+		}
+		
+		stmt.close();
+		
+		return null;
+	}
+	
+}
