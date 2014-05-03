271501e6992a93b0f997652cc86a44f7e04e7486
matmello <mat.mello93@gmail.com>
2014-05-03 02:11:20 -0300

    Adicionando DAOS: Regiao e TipoAbastecimentoAgua

---
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/dao/RegiaoDAO.java
@@ -0,0 +1,43 @@
+package br.com.mdsgpp.guiaescolaideal.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.mdsgpp.guiaescolaideal.model.Regiao;
+
+public class RegiaoDAO {
+
+	private Connection connection;
+	
+	public RegiaoDAO(Connection connection){
+		this.connection = connection;
+	}
+	
+	public Regiao pesquisarPorId(int id) throws SQLException{
+		
+		String sql = "select * from regiao where COD_REGIAO = ?";
+		
+		PreparedStatement stmt = this.connection.prepareStatement(sql);
+		stmt.setInt(1, id);
+		
+		ResultSet rs = stmt.executeQuery();
+		
+		if(rs.next()){
+			
+			Regiao regiao = new Regiao();
+			
+			regiao.setNome(rs.getString("DESCRICAO"));
+			
+			stmt.close();
+			
+			return tipoabastecimentoagua;
+		}
+		
+		stmt.close();
+		
+		return null;
+	}
+	
+}
