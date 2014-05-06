1659f9c37f169e7578e208c9f9895e5fb8fa350a
Nanson Augusto <nanson.augusto@hotmail.com>
2014-05-06 18:49:25 -0300

    Refatoração + testes do ConversorDeEntrada.

--- src/main/java/br/com/mdsgpp/guiaescolaideal/util/ConversorDeEntrada.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/util/ConversorDeEntrada.java
@@ -4,16 +4,28 @@ import java.sql.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ConversorDeEntrada {
-
+	
+	//Quebra a frase em n palavras as colocando em uma Lista.
 	public static List<String> getPalavrasChaveDoTexto(String texto) {
-		String novoTexto = texto.replaceAll(
-				"[-!\"#$%&'()*+,./:;<=>?@\\\\\\_`{|}~]", "");
-
+		
+		if(texto == null || texto.isEmpty()){
+			return new ArrayList<String>();
+		}
+		
+		String novoTexto = texto.replaceAll("[-!\"#$%&'()*+,./:;<=>?@\\\\\\_`{|}~]", "");
+		
+		//retira espaço duplo
+		novoTexto = novoTexto.replaceAll("  ", " ");
+		//retira espaço antes e no final da frase
+		novoTexto = novoTexto.trim();
+		//quebra a frase com base nos espaços
 		List<String> lista = Arrays.asList(novoTexto.split(" "));
+		
 		return lista;
 
 	}
@@ -28,9 +40,10 @@ public class ConversorDeEntrada {
 		String novoTexto = numero.replaceAll("[.]", "").trim();
 		return Integer.parseInt(novoTexto);
 	}
-
+	
+	//sim = true e false para o resto
 	public static boolean getValorBooleanDoTexto(String texto) {
-		if (texto == null) {
+		if (texto == null || texto.isEmpty()) {
 			return false;
 		} else
 			return texto.equalsIgnoreCase("sim");
@@ -38,7 +51,7 @@ public class ConversorDeEntrada {
 
 	public static Date getData(String dataTexto) throws ParseException {
 		Date data = null;
-		if (dataTexto != null) {
+		if (dataTexto != null && !dataTexto.isEmpty()) {
 			DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 			data = new Date(dateFormat.parse(dataTexto).getTime());
 		}
---
+++ src/test/java/TesteConversorDeEntrada.java
@@ -0,0 +1,117 @@
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.mdsgpp.guiaescolaideal.util.ConversorDeEntrada;
+
+
+public class TesteConversorDeEntrada {
+
+	@Test
+	public void testGetPalavrasChaveDoTextoIsNull() {
+		List<String> lista = ConversorDeEntrada.getPalavrasChaveDoTexto(null);
+		assertTrue(lista.isEmpty());
+	}
+	
+	@Test
+	public void testGetPalavrasChaveDoTextoIsEmpty() {
+		List<String> lista = ConversorDeEntrada.getPalavrasChaveDoTexto("");
+		assertTrue(lista.isEmpty());
+	}
+	
+	@Test
+	public void testGetPalavrasChaveDoTexto() {
+		List<String> lista = ConversorDeEntrada.getPalavrasChaveDoTexto("MDS + GPP");
+		assertTrue(lista.size() == 2);
+	}
+	
+	@Test
+	public void testGetPalavrasChaveDoTextoComSimbolos() {
+		List<String> lista = ConversorDeEntrada.getPalavrasChaveDoTexto("m+at*e(m)a%t#i!c@a");
+		assertTrue(lista.size() == 1);
+		assertTrue(lista.get(0).equalsIgnoreCase("Matematica"));
+	}
+
+	@Test
+	public void testGetNumeroInteiroSemPonto() {
+		String numeroTexto = "1.223.456";
+		int numero = ConversorDeEntrada.getNumeroInteiroSemPonto(numeroTexto);
+		assertTrue(numero == 1223456);
+	}
+	
+	@Test
+	public void testGetNumeroInteiroSemPontoIsEmpty() {
+		String numeroTexto = "";
+		int numero = ConversorDeEntrada.getNumeroInteiroSemPonto(numeroTexto);
+		assertTrue(numero == 0);
+	}
+
+	@Test
+	public void testGetNumeroInteiroSemPontoIsNULL() {
+		String numeroTexto = null;
+		int numero = ConversorDeEntrada.getNumeroInteiroSemPonto(numeroTexto);
+		assertTrue(numero == 0);
+	}
+	
+	@Test
+	public void testGetNumeroInteiroSemPontoComEspaco() {
+		String numeroTexto = " 1.245";
+		int numero = ConversorDeEntrada.getNumeroInteiroSemPonto(numeroTexto);
+		assertTrue(numero == 1245);
+	}
+	
+	@Test
+	public void testGetNumeroInteiroSemPontoComEspacoNoFinal() {
+		String numeroTexto = " 1.245 ";
+		int numero = ConversorDeEntrada.getNumeroInteiroSemPonto(numeroTexto);
+		assertTrue(numero == 1245);
+	}
+	
+	@Test
+	public void testGetValorBooleanDoTextoIsEmpty() {
+		assertFalse(ConversorDeEntrada.getValorBooleanDoTexto(""));
+	}
+	
+	@Test
+	public void testGetValorBooleanDoTextoIsNULL() {
+		assertFalse(ConversorDeEntrada.getValorBooleanDoTexto(""));
+	}
+
+	@Test
+	public void testGetValorBooleanDoTexto() {
+		assertFalse(ConversorDeEntrada.getValorBooleanDoTexto("nao"));
+	}
+	
+	@Test
+	public void testGetValorBooleanDoTextoValorTrue() {
+		assertTrue(ConversorDeEntrada.getValorBooleanDoTexto("Sim"));
+	}
+
+	@Test
+	public void testGetDataIsNull() throws ParseException {
+		assertNull(ConversorDeEntrada.getData(null));
+	}
+	
+	@Test
+	public void testGetDataIsEmpty() throws ParseException {
+		assertNull(ConversorDeEntrada.getData(null));
+	}
+	
+	@Test(expected=ParseException.class)
+	public void testGetDataErrada() throws ParseException {
+		assertNull(ConversorDeEntrada.getData("22-1993"));
+	}
+
+	@Test
+	public void testGetData() throws ParseException {
+		assertNotNull(ConversorDeEntrada.getData("05/05/2015"));
+	}
+	
+	@Test
+	public void testConstrutor(){
+		assertNotNull(new ConversorDeEntrada());
+	}
+}
