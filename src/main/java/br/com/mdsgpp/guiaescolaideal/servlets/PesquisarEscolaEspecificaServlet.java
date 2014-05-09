e073149214e6f0ebd888bd73db507b798d21c9cc
viniband1 <bandeiravinicius0@live.com>
2014-05-08 21:45:22 -0300

    Adequando servelet para paginaçao

--- src/main/java/br/com/mdsgpp/guiaescolaideal/servlets/PesquisarEscolaEspecificaServlet.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/servlets/PesquisarEscolaEspecificaServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import br.com.mdsgpp.guiaescolaideal.control.EscolaControl;
 import br.com.mdsgpp.guiaescolaideal.dao.ConnectionFactory;
 import br.com.mdsgpp.guiaescolaideal.dao.EscolaDAO;
+import br.com.mdsgpp.guiaescolaideal.model.Pagina;
 import br.com.mdsgpp.guiaescolaideal.util.ConversorDeEntrada;
 
 @WebServlet(value="/realizarConsultaEscolaEspecifica.jsp")
@@ -30,10 +31,32 @@ public class PesquisarEscolaEspecificaServlet extends HttpServlet {
 		String nome = null;
 		String estado = null;
 		String municipio = null;
+		String pagAtual = null;
+		String escAtual = null;
 
 		nome = request.getParameter("nome");
 		estado = request.getParameter("estado");
 		municipio = request.getParameter("municipio");
+		pagAtual = request.getParameter("pagatual");
+		escAtual = request.getParameter("escatual");
+		
+		Pagina pagina = new Pagina();
+		
+		pagina.setNome(nome);
+		pagina.setEstado(estado);
+		pagina.setMunicipio(municipio);
+		pagina.setMaxResult(20);
+		
+		
+		int pag = ConversorDeEntrada.getNumeroInteiroSemPonto(pagAtual);
+		if(pag == 0)
+		    pag = 1;
+		
+		pagina.setPagAtual(pag);
+		
+		int esc = ConversorDeEntrada.getNumeroInteiroSemPonto(escAtual);
+		
+		pagina.setEscAtual(esc);
 		
 		Connection con = null;
 		
@@ -42,13 +65,12 @@ public class PesquisarEscolaEspecificaServlet extends HttpServlet {
 			EscolaDAO escolaDAO = new EscolaDAO(con);
 			EscolaControl escolaControl = new EscolaControl(escolaDAO);
 			
-			List<String> palavras = ConversorDeEntrada.getPalavrasChaveDoTexto(nome);
-			List<String> palavrasMunicipio = ConversorDeEntrada.getPalavrasChaveDoTexto(municipio);
-			
-			request.setAttribute("listaEscola",
-					escolaControl.getEscolaEspecifica(palavras, estado, palavrasMunicipio));
+			pagina.setListaEscola(escolaControl.getEscolaEspecifica(pagina));
+			request.setAttribute("pagina",
+					pagina);
 
 			dispatcher = request.getRequestDispatcher("/resultadoPesquisa.jsp");
+			con.close();
 		} catch (SQLException e) {
 			dispatcher = setDispatcherErro(request, e);
 		} catch (ParseException e) {
