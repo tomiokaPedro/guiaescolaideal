7e9fda74d55471d9a478e50eda092da7bc9d8500
Nanson Augusto <nanson.augusto@hotmail.com>
2014-05-14 22:29:59 -0300

    Add atributo localhost.

--- src/main/java/br/com/mdsgpp/guiaescolaideal/dao/ConnectionFactory.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/dao/ConnectionFactory.java
@@ -9,14 +9,15 @@ public class ConnectionFactory {
 	private String user = "root";
 	private String password = "";
 	private String database = "guia_escola_ideal";
+	private String localhost = "jdbc:mysql://" + "localhost/";
 
 	public Connection getConnection() throws SQLException {
 		Connection connection = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/"
-					+ database, user, password);
+			connection = DriverManager.getConnection(localhost + database,
+					user, password);
 		} catch (ClassNotFoundException e) {
 			throw new SQLException();
 		}
