bee165b30fdcea3ed007fef291c5765e3841e982
Nanson Augusto <nanson.augusto@hotmail.com>
2014-06-02 16:50:05 -0300

    Add teste municipio.

--- src/test/java/br/com/mdsgpp/guiaescolaideal/aceitacao/teste/TesteAceitacaoFormEscolaIdeal.java
+++ src/test/java/br/com/mdsgpp/guiaescolaideal/aceitacao/teste/TesteAceitacaoFormEscolaIdeal.java
@@ -12,9 +12,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class TesteAceitacaoFormEscolaIdeal {
-    
+
     private WebDriver driver;
-    
+
     @Before
     public void setUp() throws Exception {
 	driver = new FirefoxDriver();
@@ -23,48 +23,77 @@ public class TesteAceitacaoFormEscolaIdeal {
 
     @Test
     public void testeCasoPerfeito() {
-	
+
 	String linkPag = driver.getCurrentUrl();
-	
-	Select listaModalidade = new Select(driver.findElement(By.name("modalidade")));
+
+	Select listaModalidade = new Select(driver.findElement(By
+		.name("modalidade")));
 	listaModalidade.selectByVisibleText("Ensino Regular");
-	
+
 	Select listaEstado = new Select(driver.findElement(By.name("estado")));
 	listaEstado.selectByVisibleText("Distrito Federal");
-	
+
+	WebElement radioLabInfo = driver.findElement(By.name("labinf"));
+	radioLabInfo.click();
+
+	WebElement radioLabCiencia = driver.findElement(By.name("labcien"));
+	radioLabCiencia.click();
+
+	WebElement submit = driver.findElement(By.name("submit"));
+	submit.click();
+
+	pausa();
+
+	String linkPagAtual = driver.getCurrentUrl();
+
+	assertTrue(!linkPag.equalsIgnoreCase(linkPagAtual));
+    }
+
+    @Test
+    public void testeSemInserirEstado() {
+	Select listaModalidade = new Select(driver.findElement(By
+		.name("modalidade")));
+	listaModalidade.selectByVisibleText("Ensino Regular");
+
+	WebElement submit = driver.findElement(By.name("submit"));
+	submit.click();
+
+	String texto = driver.switchTo().alert().getText();
+
+	driver.switchTo().alert().accept();
+
+	assertTrue(texto.equals("Selecione um estado"));
+
+    }
+    
+    @Test
+    public void testeMunicipioComCaracteresInvalidos(){
+
+	String linkPag = driver.getCurrentUrl();
+
+	Select listaModalidade = new Select(driver.findElement(By
+		.name("modalidade")));
+	listaModalidade.selectByVisibleText("Ensino Regular");
+
+	Select listaEstado = new Select(driver.findElement(By.name("estado")));
+	listaEstado.selectByVisibleText("Distrito Federal");
+
 	WebElement radioLabInfo = driver.findElement(By.name("labinf"));
 	radioLabInfo.click();
 
 	WebElement radioLabCiencia = driver.findElement(By.name("labcien"));
 	radioLabCiencia.click();
 	
-	WebElement submit  = driver.findElement(By.name("submit"));
+	WebElement municipio = driver.findElement(By.name("municipio"));
+	municipio.sendKeys("$%$и&%иии&%");
+
+	WebElement submit = driver.findElement(By.name("submit"));
 	submit.click();
-	
-	pausa();
-	
-	String linkPagAtual = driver.getCurrentUrl();
-	
-	assertTrue(!linkPag.equalsIgnoreCase(linkPagAtual));
-    }
-    
-    @Test
-    public void testeSemInserirEstado()
-    {
-	Select listaModalidade = new Select(driver.findElement(By.name("modalidade")));
-	listaModalidade.selectByVisibleText("Ensino Regular");
-	
-	WebElement submit  = driver.findElement(By.name("submit"));
-	submit.click();
-	
-	
+
 	String texto = driver.switchTo().alert().getText();
-	
 	driver.switchTo().alert().accept();
-	
-	assertTrue(texto.equals("Selecione um estado"));
-	
-	
+
+	assertTrue(texto != null && !texto.isEmpty());
     }
 
     private void pausa() {
@@ -73,10 +102,10 @@ public class TesteAceitacaoFormEscolaIdeal {
 	} catch (InterruptedException e) {
 	}
     }
-    
+
     @After
-    public void close(){
+    public void close() {
 	driver.close();
     }
-    
+
 }
