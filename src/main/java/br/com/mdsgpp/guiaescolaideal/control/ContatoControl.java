3d45bfb28b5578792493cc9ba1d2b80cbc3e426b
Felipe Marques <felipesampaio93@hotmail.com>
2014-06-22 17:13:58 -0300

    criacao da classe ContatoControl

---
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/control/ContatoControl.java
@@ -0,0 +1,55 @@
+package br.com.mdsgpp.guiaescolaideal.control;
+
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class ContatoControl
+{
+      public void enviarEmail(String nome, String email, String assunto, String mensagem) {
+            Properties props = new Properties();
+            /** Parâmetros de conexão com servidor Gmail */
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+
+            Session session = Session.getDefaultInstance(props,
+                        new javax.mail.Authenticator() {
+                             protected PasswordAuthentication getPasswordAuthentication() 
+                             {
+                                   return new PasswordAuthentication("contato.guiaescolaideal@gmail.com", "4t6Li40vZt");
+                             }
+                        });
+
+            /** Ativa Debug para sessão */
+            session.setDebug(true);
+
+            try {
+
+                  Message message = new MimeMessage(session);
+                  message.setFrom(new InternetAddress("contato.guiaescolaideal@gmail.com")); //Remetente
+
+                  Address[] toUser = InternetAddress //Destinatário(s)
+                             .parse("contato.guiaescolaideal@gmail.com");  
+
+                  message.setRecipients(Message.RecipientType.TO, toUser);
+                  message.setSubject(assunto);//Assunto
+                  message.setText(mensagem);
+                  /**Método para enviar a mensagem criada*/
+                  Transport.send(message);
+
+                  
+
+             } catch (MessagingException e) {
+                  throw new RuntimeException(e);
+            }
+      }
+}
