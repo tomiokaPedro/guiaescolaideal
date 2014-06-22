1c715bf832d70001058de4c85873b2f1652ddcac
Nanson Augusto <nanson.augusto@hotmail.com>
2014-06-22 18:35:11 -0300

    outros testes.

--- src/test/java/br/com/mdsgpp/guiaescolaideal/servlets/teste/TesteRemovePerfilServlet.java
+++ src/test/java/br/com/mdsgpp/guiaescolaideal/servlets/teste/TesteRemovePerfilServlet.java
@@ -1,7 +1,7 @@
 package br.com.mdsgpp.guiaescolaideal.servlets.teste;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -19,109 +19,80 @@ import org.junit.Test;
 import br.com.mdsgpp.guiaescolaideal.exceptions.EscolaRepetidaException;
 import br.com.mdsgpp.guiaescolaideal.exceptions.QuantidadeDeEscolasExcedenteException;
 import br.com.mdsgpp.guiaescolaideal.model.Escola;
-import br.com.mdsgpp.guiaescolaideal.servlets.AddPerfilServlet;
+import br.com.mdsgpp.guiaescolaideal.servlets.RemovePerfilServlet;
 import br.com.mdsgpp.guiaescolaideal.servlets.teste.dao.TesteDAO;
 import br.com.mdsgpp.guiaescolaideal.util.Compara;
 
-public class TesteComparaPerfisServlet {
+public class TesteRemovePerfilServlet {
+
     private HttpServletRequest request;
     private HttpServletResponse response;
     private RequestDispatcher dispatcherCompara;
     private RequestDispatcher dispatcherErro;
     private HttpSession session;
     private Compara compara;
-    
+
     private TesteDAO testeDAO;
-    private AddPerfilServlet comparaPerfisServlet;
-    
+    private RemovePerfilServlet perfilServlet;
+
     @Before
     public void inicializaComponentesBasicos() throws Exception {
 	request = mock(HttpServletRequest.class);
 	response = mock(HttpServletResponse.class);
-	
-	dispatcherCompara= mock(RequestDispatcher.class);
+
+	dispatcherCompara = mock(RequestDispatcher.class);
 	dispatcherErro = mock(RequestDispatcher.class);
-	
+
 	session = mock(HttpSession.class);
-	
+
 	compara = new Compara();
-	
+
 	when(session.getAttribute("compara")).thenReturn(compara);
 	when(request.getSession()).thenReturn(session);
-	
-	comparaPerfisServlet = new AddPerfilServlet();
-	
+
+	perfilServlet = new RemovePerfilServlet();
+
 	addEscolasNoBanco();
     }
 
     @Test
-    public void cenarioAddUmaEscola() throws ServletException, IOException {
-	addParameterId("75015");
+    public void cenarioRemoveEscola() throws QuantidadeDeEscolasExcedenteException, EscolaRepetidaException, ServletException, IOException {
+	addEscola(17451);
+	addParameterId("17451");
 	configuraDispatcherParaPaginaComparaPerfis();
 	
-	assertNull(compara.getEscolaA());
-	
-	comparaPerfisServlet.service(request, response);
+	perfilServlet.service(request, response);
 	
 	verify(dispatcherCompara).forward(request, response);
-	assertNotNull(compara.getEscolaA());
     }
     
     @Test
-    public void cenarioComUmaEscolaExistente() throws ServletException, IOException, QuantidadeDeEscolasExcedenteException, EscolaRepetidaException{
-	addEscola();
-	addParameterId("75015");
-	configuraDispatcherParaPaginaComparaPerfis();
-	
-	assertNotNull(compara.getEscolaA());
-	assertNull(compara.getEscolaB());
-	
-	comparaPerfisServlet.service(request, response);
-	
-	verify(dispatcherCompara).forward(request, response);
-	
-	assertNotNull(compara.getEscolaA());
-	assertNotNull(compara.getEscolaB());
-    }
-    
-    @Test
-    public void cenarioAdicionandoUmaEscolaJaExistente() throws ServletException, IOException, QuantidadeDeEscolasExcedenteException, EscolaRepetidaException{
-	addEscola(75015);
-	addParameterId("75015");
+    public void cenarioRemoveEscolaInexistente() throws QuantidadeDeEscolasExcedenteException, EscolaRepetidaException, ServletException, IOException {
+	addEscola(17451);
+	addParameterId("45687");
 	configuraDispatcherParaPaginaDeErro();
 	
-	assertNotNull(compara.getEscolaA());
-	assertNull(compara.getEscolaB());
+	perfilServlet.service(request, response);
 	
-	comparaPerfisServlet.service(request, response);
-	
+	verify(dispatcherCompara, never()).forward(request, response);
 	verify(dispatcherErro).forward(request, response);
-	
-	assertNotNull(compara.getEscolaA());
-	assertNull(compara.getEscolaB());
     }
     
     @Test
-    public void cenarioComDuasEscolas() throws ServletException, IOException, QuantidadeDeEscolasExcedenteException, EscolaRepetidaException{
-	addEscola(1234);
-	addEscola(4321);
-	
-	addParameterId("75015");
+    public void cenarioSessionSemAtributo() throws QuantidadeDeEscolasExcedenteException, EscolaRepetidaException, ServletException, IOException {
+	when(session.getAttribute("compara")).thenReturn(null);
 	configuraDispatcherParaPaginaDeErro();
 	
-	assertNotNull(compara.getEscolaA());
-	assertNotNull(compara.getEscolaB());
+	perfilServlet.service(request, response);
 	
-	comparaPerfisServlet.service(request, response);
-	
+	verify(dispatcherCompara, never()).forward(request, response);
 	verify(dispatcherErro).forward(request, response);
-	
-	assertNotNull(compara.getEscolaA());
-	assertNotNull(compara.getEscolaB());
     }
-    
-    private void addEscola() throws QuantidadeDeEscolasExcedenteException, EscolaRepetidaException {
-	compara.add(mock(Escola.class));
+
+
+    private void addEscolasNoBanco() {
+	testeDAO = new TesteDAO("Teste Servlet");
+	testeDAO.run();
     }
     
     private void addEscola(int id) throws QuantidadeDeEscolasExcedenteException, EscolaRepetidaException {
@@ -129,14 +100,9 @@ public class TesteComparaPerfisServlet {
 	when(escola.getCodEscola()).thenReturn(id);
 	compara.add(escola);
     }
-
+    
     private void addParameterId(String id) {
 	when(request.getParameter("id")).thenReturn(id);
-    }
-    
-    private void addEscolasNoBanco() {
-	testeDAO = new TesteDAO("Teste Servlet");
-	testeDAO.run();
     }
     
     private void configuraDispatcherParaPaginaDeErro() {
@@ -146,5 +112,4 @@ public class TesteComparaPerfisServlet {
     private void configuraDispatcherParaPaginaComparaPerfis() {
 	when(request.getRequestDispatcher("/comparaPerfis.jsp")).thenReturn(dispatcherCompara);
     }
-
 }
