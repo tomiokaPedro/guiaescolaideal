993df34f1b6d00f96d08a856da72df53cf033162
Felipe Marques <felipesampaio93@hotmail.com>
2014-06-26 17:43:57 -0300

    exception da contato

--- src/main/java/br/com/mdsgpp/guiaescolaideal/exceptions/EnvioException.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/exceptions/EnvioException.java
@@ -2,6 +2,8 @@ package br.com.mdsgpp.guiaescolaideal.exceptions;
 
 public class EnvioException extends ContatoException 
 {
+    private static final long serialVersionUID = 1L;
+
     public EnvioException(String mensagem)
     {
 	super(mensagem);
