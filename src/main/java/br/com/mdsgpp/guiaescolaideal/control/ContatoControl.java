650094fffff6c295cc3d2ed8a683f538fca4a461
Nanson Augusto <nanson.augusto@hotmail.com>
2014-06-25 22:37:20 -0300

    Pequena refatoração na ControlContato.

--- src/main/java/br/com/mdsgpp/guiaescolaideal/control/ContatoControl.java
+++ src/main/java/br/com/mdsgpp/guiaescolaideal/control/ContatoControl.java
@@ -8,12 +8,11 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;  
 import javax.mail.internet.MimeMessage;   
 import javax.mail.Authenticator;  
-import javax.mail.PasswordAuthentication;   
-
-import com.mysql.fabric.Response;
+import javax.mail.PasswordAuthentication;
   
 public class ContatoControl {  
-      
+    
+    private Session session;
     private String mailSMTPServer;  
     private String mailSMTPServerPort;  
       
@@ -24,6 +23,8 @@ public class ContatoControl {
     public ContatoControl() { //Para o GMAIL   
         mailSMTPServer = "smtp.gmail.com";  
         mailSMTPServerPort = "465";  
+        
+        this.session = configuraSession();
     }  
     /* 
      * caso queira mudar o servidor e a porta, so enviar para o contrutor 
@@ -31,42 +32,17 @@ public class ContatoControl {
      */  
     public ContatoControl(String mailSMTPServer, String mailSMTPServerPort) { //Para outro Servidor  
         this.mailSMTPServer = mailSMTPServer;  
-        this.mailSMTPServerPort = mailSMTPServerPort;  
-    }  
-      
-    public void sendMail(String from, String to, String subject, String message) {  
-          
-        Properties props = new Properties();  
+        this.mailSMTPServerPort = mailSMTPServerPort; 
         
-                // quem estiver utilizando um SERVIDOR PROXY descomente essa parte e atribua as propriedades do SERVIDOR PROXY utilizado  
-                /* 
-                props.setProperty("proxySet","true"); 
-                props.setProperty("socksProxyHost","192.168.155.1"); // IP do Servidor Proxy 
-                props.setProperty("socksProxyPort","1080");  // Porta do servidor Proxy 
-                */  
-  
-        props.put("mail.transport.protocol", "smtp"); //define protocolo de envio como SMTP  
-        props.put("mail.smtp.starttls.enable","true");   
-        props.put("mail.smtp.host", mailSMTPServer); //server SMTP do GMAIL  
-        props.put("mail.smtp.auth", "true"); //ativa autenticacao  
-        props.put("mail.smtp.user", from); //usuario ou seja, a conta que esta enviando o email (tem que ser do GMAIL)  
-        props.put("mail.debug", "true");  
-        props.put("mail.smtp.port", mailSMTPServerPort); //porta  
-        props.put("mail.smtp.socketFactory.port", mailSMTPServerPort); //mesma porta para o socket  
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");  
-        props.put("mail.smtp.socketFactory.fallback", "false");  
-          
-        //Cria um autenticador que sera usado a seguir  
-        SimpleAuth auth = null;  
-        auth = new SimpleAuth ("contato.guiaescolaideal@gmail.com","4t6Li40vZt");  
-          
-        //Session - objeto que ira realizar a conexão com o servidor  
-        /*Como há necessidade de autenticação é criada uma autenticacao que 
-         * é responsavel por solicitar e retornar o usuário e senha para  
-         * autenticação */  
-        Session session = Session.getDefaultInstance(props, auth);  
-        session.setDebug(true); //Habilita o LOG das ações executadas durante o envio do email  
-  
+        this.session = configuraSession();
+    }  
+     
+    public ContatoControl(Session session){
+	this.session = session;
+    }
+    
+    public void sendMail(String from, String to, String subject, String message) {  
+        session.getProperties().put("mail.smtp.user", from); //usuario ou seja, a conta que esta enviando o email (tem que ser do GMAIL)
         //Objeto que contém a mensagem  
         Message msg = new MimeMessage(session);  
   
@@ -105,6 +81,38 @@ public class ContatoControl {
             e.printStackTrace();  
         }  
   
+    }
+    private Session configuraSession() {
+	Properties props = new Properties();  
+        
+                // quem estiver utilizando um SERVIDOR PROXY descomente essa parte e atribua as propriedades do SERVIDOR PROXY utilizado  
+                /* 
+                props.setProperty("proxySet","true"); 
+                props.setProperty("socksProxyHost","192.168.155.1"); // IP do Servidor Proxy 
+                props.setProperty("socksProxyPort","1080");  // Porta do servidor Proxy 
+                */  
+  
+        props.put("mail.transport.protocol", "smtp"); //define protocolo de envio como SMTP  
+        props.put("mail.smtp.starttls.enable","true");   
+        props.put("mail.smtp.host", mailSMTPServer); //server SMTP do GMAIL  
+        props.put("mail.smtp.auth", "true"); //ativa autenticacao  
+        props.put("mail.debug", "true");  
+        props.put("mail.smtp.port", mailSMTPServerPort); //porta  
+        props.put("mail.smtp.socketFactory.port", mailSMTPServerPort); //mesma porta para o socket  
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");  
+        props.put("mail.smtp.socketFactory.fallback", "false");  
+          
+        //Cria um autenticador que sera usado a seguir  
+        SimpleAuth auth = null;  
+        auth = new SimpleAuth ("contato.guiaescolaideal@gmail.com","4t6Li40vZt");  
+          
+        //Session - objeto que ira realizar a conexão com o servidor  
+        /*Como há necessidade de autenticação é criada uma autenticacao que 
+         * é responsavel por solicitar e retornar o usuário e senha para  
+         * autenticação */  
+        Session session = Session.getDefaultInstance(props, auth);  
+        session.setDebug(true); //Habilita o LOG das ações executadas durante o envio do email  
+	return session;
     }  
 }  
   
